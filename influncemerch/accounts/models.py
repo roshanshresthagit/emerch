d958525b46fe96820ee9bcc7fba3568962bcdaed
Roshan <roshanshrestha0921@gmail.com>
2024-02-11 21:59:55 +0545

    commit for custom user

--- influncemerch/accounts/models.py
+++ influncemerch/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin 
 from django.db import models
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+        email = models.EmailField(_("email address"), unique=True)
+
+        objects = CustomUserManager()
+
+   
+    
+
