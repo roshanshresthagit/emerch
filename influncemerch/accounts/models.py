45ca2dbfb7467e9e7be0cf319899567e4ef324a2
Roshan <roshanshrestha0921@gmail.com>
2024-02-17 21:34:37 +0545

    product model

--- influncemerch/accounts/models.py
+++ influncemerch/accounts/models.py
@@ -4,7 +4,6 @@ from .managers import CustomUserManager
 from django.utils import timezone
 import uuid 
 import hashlib
-from django.urls import reverse
 
 
 GENDER_CHOICES = [
@@ -45,47 +44,4 @@ class User(AbstractBaseUser, PermissionsMixin):
         
         def get_address(self):
                 return self.address
-
-
-class Category(models.Model):
-        cat_id=models.AutoField(primary_key=True)
-        title=models.CharField(max_length=64)
-        description = models.CharField(max_length=100)
-        url_slug=models.CharField(max_length=255)
-        thumbnail=models.FileField()
-        created_at=models.DateTimeField(auto_now_add=True)
-
-        @staticmethod
-        def get_all_category(self):
-                return Category.objects.all()
-        
-        def __str__(self):
-                return self.title
-        
-class SubCategories(models.Model):
-        id=models.AutoField(primary_key=True)
-        category_id=models.ForeignKey(Category,on_delete=models.CASCADE)
-        title=models.CharField(max_length=255)
-        url_slug=models.CharField(max_length=255)
-        thumbnail=models.FileField()
-        description=models.TextField()
-        created_at=models.DateTimeField(auto_now_add=True)
-
-        def get_absolute_url(self):
-                return reverse("sub_category_list")
-
-class Product(models.Model):
-        id=models.AutoField(primary_key=True)
-        url_slug=models.CharField(max_length=255)
-        subcategories_id=models.ForeignKey(SubCategories,on_delete=models.CASCADE)
-        product_name=models.CharField(max_length=255)
-        brand=models.CharField(max_length=255)
-        product_max_price=models.CharField(max_length=255)
-        product_discount_price=models.CharField(max_length=255)
-        product_description=models.TextField()
-        product_long_description=models.TextField()
-        created_at=models.DateTimeField(auto_now_add=True)
-        in_stock_total=models.IntegerField(default=1)
-   
-    
 
