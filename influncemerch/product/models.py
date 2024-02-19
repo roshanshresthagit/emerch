7ebc1e639aa64119dba31beb81b4ba07b2e0ef50
Roshan <roshanshrestha0921@gmail.com>
2024-02-19 21:35:12 +0545

    model changes

--- influncemerch/product/models.py
+++ influncemerch/product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from accounts.models import User
 
 class Category(models.Model):
         cat_id=models.AutoField(primary_key=True)
@@ -29,7 +29,7 @@ class SubCategories(models.Model):
         def get_absolute_url(self):
                 return reverse("sub_category_list")
 
-class Product(models.Model):
+class Products(models.Model):
         id=models.AutoField(primary_key=True)
         url_slug=models.CharField(max_length=255)
         subcategories_id=models.ForeignKey(SubCategories,on_delete=models.CASCADE)
@@ -40,7 +40,88 @@ class Product(models.Model):
         product_description=models.TextField()
         product_long_description=models.TextField()
         created_at=models.DateTimeField(auto_now_add=True)
+        #     added_by_merchant=models.ForeignKey(MerchantUser,on_delete=models.CASCADE)
         in_stock_total=models.IntegerField(default=1)
-   
+        is_active=models.IntegerField(default=1)
+
+class ProductMedia(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        media_type_choice=((1,"Image"),(2,"Video"))
+        media_type=models.CharField(max_length=255)
+        media_content=models.FileField()
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)  
+
+class ProductTransaction(models.Model):
+        id=models.AutoField(primary_key=True)
+        transaction_type_choices=((1,"BUY"),(2,"SELL"))
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        transaction_product_count=models.IntegerField(default=1)
+        transaction_type=models.CharField(choices=transaction_type_choices,max_length=255)
+        transaction_description=models.CharField(max_length=255)
+        created_at=models.DateTimeField(auto_now_add=True)
+
+
+class ProductDetails(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        title=models.CharField(max_length=255)
+        title_details=models.CharField(max_length=255)
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)
+
+class ProductAbout(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        title=models.CharField(max_length=255)
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)
+
+class ProductTags(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        title=models.CharField(max_length=255)
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)
+
+class ProductQuestions(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+        question=models.TextField()
+        answer=models.TextField()
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)
+
+class ProductReviews(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+        review_image=models.FileField()
+        rating=models.CharField(default="5",max_length=255)
+        review=models.TextField(default="")
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)
+
+class ProductReviewVoting(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_review_id=models.ForeignKey(ProductReviews,on_delete=models.CASCADE)
+        user_id_voting=models.ForeignKey(User,on_delete=models.CASCADE)
+        created_at=models.DateTimeField(auto_now_add=True)
+        is_active=models.IntegerField(default=1)
+
+class ProductVarient(models.Model):
+        id=models.AutoField(primary_key=True)
+        title=models.CharField(max_length=255)
+        created_at=models.DateTimeField(auto_now_add=True)
+
+class ProductVarientItems(models.Model):
+        id=models.AutoField(primary_key=True)
+        product_varient_id=models.ForeignKey(ProductVarient,on_delete=models.CASCADE)
+        product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+        title=models.CharField(max_length=255)
+        created_at=models.DateTimeField(auto_now_add=True)
+
     
 
